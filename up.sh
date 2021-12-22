b90049a558ebffc778a01504dd64722f91689797
wiqram <wiqram@live.com>
2021-12-22 23:03:52 +0000

    qcguy now on k8s and config files mapped within container

--- up.sh
+++ up.sh
@@ -12,6 +12,15 @@ if kubectl version; then
     echo "Command failed"
     minikube start --cpus 4 --memory 16384
     #minikube start
+    #set strictARP to true to allow for MetalLB loadbalancer
+    #kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: false/strictARP: false/" | kubectl apply -f - -n kube-system
+    #To install MetalLB, apply the manifest
+    #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
+    #kubectl apply -f metallb.yaml
+    #This will deploy MetalLB to your cluster, under the metallb-system namespace
+    #now install metallb config
+    #kubectl apply -f metallb-config.yaml
+    #
 fi
 #allow minikube to connect to local docker images
 eval $(minikube -p minikube docker-env)
